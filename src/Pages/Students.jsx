e2c257bc0f37417c129e439a681ac5e7b4107786
pramodkumar0 <pramodpusala@gmail.com>
2022-11-03 19:25:36 +0530

    message

---
+++ src/Pages/Students.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import StripedRowExample from './Table.jsx'
+
+export default function Students() {
+  return (
+    
+    <div className='container'>
+    
+    <div className='text'>Students</div>
+
+    <button className='textt' >Add new Student</button> 
+    <br/>
+    
+    <StripedRowExample/>
+   
+
+
+    </div>
+  )
+}
